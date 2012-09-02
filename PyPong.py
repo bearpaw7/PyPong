5a5ee1c09336dc35ae30cacebf18021804119b73
beef <met898@tamu.edu>
2012-09-02 16:59:56 -0500

    Time's up and it's broken.

--- PyPong.py
+++ PyPong.py
@@ -52,187 +52,121 @@ class Ball:
         return self.position[1]-self.radius
     def right(self):
         return self.position[1]+self.radius
-    def update(self):
+    def collide(self, paddle):
+        if self.left()>=paddle.left() and self.right()<=paddle.right():
+            if self.top()>=paddle.top() and self.bottom()<=paddle.bottom():
+                self.direction+=150
+                self.velocity=min((self.velocity+self.velocity*0.20),paddle.width)
+                if paddle.color == red:
+                    self.position[0]=paddle.right()+self.radius
+                if paddle.color == blue:
+                    self.position[0]=paddle.left()-self.radius
+    def update(self,paddle1,paddle2):
         self.position[0]+=self.velocity*math.cos(self.direction*pi/180)
         self.position[1]-=self.velocity*math.sin(self.direction*pi/180)
+
+        if self.top()<0:
+            self.direction=bounceHorizontal(self.direction)
+            self.position[1]=self.radius
+        elif self.bottom()>height:
+            self.direction=bounceHorizontal(self.direction)
+            self.position[1]=height-self.radius
+        if self.right()<0:
+            self.position=[width/2,height/2]
+            self.direction=0
+            self.velocity=1.25
+        elif self.left()>width:
+            self.position=[width/2,height/2]
+            self.direction=0
+            self.velocity=1.25
+        self.collide(paddle1)
+        self.collide(paddle2)
     def draw(self,screen):
         pygame.draw.circle(screen,black,[(int)(self.position[0]),(int)(self.position[1])],self.radius,0)
 
 class Paddle:
-    def __init__(self,x,y):
+    def __init__(self,x,y,_color):
         self.position=[x,y]
         self.width=15
         self.height=150
         self.activity=0
+        self.color=_color
     def top(self):
-        return self.position[1]
-    def bottom(self):
-        return self.position[1]+self.height
-    def left(self):
         return self.position[0]
+    def bottom(self):
+        return self.position[0]+self.height
+    def left(self):
+        return self.position[1]
     def right(self):
-        return self.position[0]+self.width
+        return self.position[1]+self.width
     def update(self,event):
-        if event.type == pygame.KEYUP and event.key == pygame.K_q:
-            self.activity=0
-        elif event.type == pygame.KEYUP and event.key == pygame.K_a:
-            self.activity=0
-        if event.type == pygame.KEYDOWN and event.key == pygame.K_q:
-            self.activity=1
-        elif event.type == pygame.KEYDOWN and event.key == pygame.K_a:
-            self.activity=-1
+        if self.color == red:
+            if event.type == pygame.KEYUP and event.key == pygame.K_q:
+                self.activity=0
+            elif event.type == pygame.KEYUP and event.key == pygame.K_a:
+                self.activity=0
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_q:
+                self.activity=1
+            elif event.type == pygame.KEYDOWN and event.key == pygame.K_a:
+                self.activity=-1
+        if self.color == blue:
+            if event.type == pygame.KEYUP and event.key == pygame.K_UP:
+                self.activity=0
+            elif event.type == pygame.KEYUP and event.key == pygame.K_DOWN:
+                self.activity=0
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_UP:
+                self.activity=1
+            elif event.type == pygame.KEYDOWN and event.key == pygame.K_DOWN:
+                self.activity=-1
+        # move player bars
+        if self.activity>0:
+            self.position[0]-=5
+        elif self.activity<0:
+            self.position[0]+=5
+    def draw(self,screen):
+        pygame.draw.line(screen,self.color,[self.left(),self.top()],[self.left(),self.bottom()],self.width)
 
 # Initialize the game engine
 pygame.init()
 
 size=[width,height]
 screen=pygame.display.set_mode(size)
- 
 pygame.display.set_caption("PyPong")
- 
+
 #Loop until the user clicks the close button.
 done=False
 clock=pygame.time.Clock()
 
-# [x,y]
-#ball=Ball(width/2,height/2)
-ball=[width/2,height/2]
-ballDirection=30
-ballRadius=10
-ballSpeed=1.25
-#
-paddleLength=100
-paddleWidth=20
-
-#player1=Paddle(40,width/2)
-#player2=Paddle(650,width/2)
-player1column=40
-player2column=650
-#
-player1top=(height/2)+ballRadius+1
-player2top=(height/2)-paddleLength-ballRadius
-
-player1movement=0
-player2movement=0
+ball=Ball(width/2,height/2,0)
+player1=Paddle(40,height/3,red)
+player2=Paddle(60,height/3,blue)
 
 while done==False:
     # This limits the while loop to a max of 10 times per second.
     # Leave this out and we will use all CPU we can.
     clock.tick(45)
-     
+
     for event in pygame.event.get(): # User did something
         # If user clicked close
         if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
             done=True # Flag that we are done so we exit this loop
-        # First player
-        if event.type == pygame.KEYUP and event.key == pygame.K_q:
-            player1movement = 0
-        elif event.type == pygame.KEYUP and event.key == pygame.K_a:
-            player1movement = 0
-        if event.type == pygame.KEYDOWN and event.key == pygame.K_q:
-            player1movement = 1
-        elif event.type == pygame.KEYDOWN and event.key == pygame.K_a:
-            player1movement = -1
-        # Second player
-        if event.type == pygame.KEYUP and event.key == pygame.K_UP:
-            player2movement = 0
-        elif event.type == pygame.KEYUP and event.key == pygame.K_DOWN:
-            player2movement = 0
-        if event.type == pygame.KEYDOWN and event.key == pygame.K_UP:
-            player2movement = 1
-        elif event.type == pygame.KEYDOWN and event.key == pygame.K_DOWN:
-            player2movement = -1
-    # move player bars
-    if player1movement > 0:
-        player1top -= 5
-    elif player1movement < 0:
-        player1top += 5
-    if player2movement > 0:
-        player2top -= 5
-    elif player2movement < 0:
-        player2top += 5
+        player1.update(event)
+        player2.update(event)
+    ball.update(player1,player2)
 
-    # All drawing code happens after the for loop and but
-    # inside the main while done==False loop.
-     
-    # Clear the screen and set the screen background
     screen.fill(white)
- 
-    # Draw on the screen a green line from (0,0) to (100,100) 
-    # 5 pixels wide.
-    pygame.draw.line(screen,red,[player1column,player1top],[player1column,player1top+paddleLength],paddleWidth)
-    pygame.draw.line(screen,blue,[player2column,player2top],[player2column,player2top+paddleLength],paddleWidth)
 
-    # Draw on the screen several green lines from (0,10) to (100,110) 
-    # 5 pixels wide using a loop
-#    y_offset=0
-#    while y_offset < 100:
-#        pygame.draw.line(screen,red,[0,10+y_offset],[100,110+y_offset],5)
-    pygame.draw.line(screen,red,[0,ball[1]],[width,ball[1]],1)
-    pygame.draw.line(screen,red,[ball[0],0],[ball[0],height],1)
-#        y_offset=y_offset+10
+    player1.draw(screen)
+    player2.draw(screen)
+    ball.draw(screen)
 
-    # Draw a rectangle
-#    pygame.draw.rect(screen,black,[20,20,250,100],2)
-     
-    # Draw an ellipse, using a rectangle as the outside boundaries
-#    pygame.draw.ellipse(screen,black,[20,20,250,100],2) 
-
-    # Draw an arc as part of an ellipse. 
-    # Use radians to determine what angle to draw.
-#    pygame.draw.arc(screen,black,[20,220,250,200], 0, pi/2, 2)
-#    pygame.draw.arc(screen,green,[20,220,250,200], pi/2, pi, 2)
-#    pygame.draw.arc(screen,blue, [20,220,250,200], pi,3*pi/2, 2)
-#    pygame.draw.arc(screen,red, [20,220,250,200],3*pi/2, 2*pi, 2)
-
-
-    if ball[0]>=player1column and ball[0]<=(player1column+paddleWidth):
-        if (ball[1]+ballRadius)>=player1top and ball[1]<=(player1top+paddleLength):
-            ballDirection+=180
-#            ballSpeed+=ballSpeed*0.20
-            ballSpeed=min((ballSpeed+ballSpeed*0.20),paddleWidth)
-            print ball,ballSpeed,paddleWidth
-            ball[0]=player1column+paddleWidth
-    if (ball[0]+2*ballRadius)>=player2column and ball[0]<=(player2column+paddleWidth):
-        if (ball[1]-ballRadius)>=player2top and ball[1]<=(player2top+paddleLength):
-            ballDirection+=180
-            ballSpeed=min((ballSpeed+ballSpeed*0.20),paddleWidth)
-            print ball,ballSpeed,paddleWidth
-            ball[0]=player2column-2*ballRadius
-    ballDirection%=360
-    if (ball[1]-ballRadius)<0:
-        ballDirection=bounceHorizontal(ballDirection)
-        ball[1]=ballRadius
-    elif (ball[1]+ballRadius)>height:
-        ballDirection=bounceHorizontal(ballDirection)
-        ball[1]=height-ballRadius
-    if ball[0]<(0-ballRadius):
-        ball=[width/2,height/2]
-        ballDirection=180
-        ballSpeed=1.0
-    elif ball[0]>(ballRadius+width):
-        ball=[width/2,height/2]
-        ballDirection=0
-        ballSpeed=1.0
-
-    ball[0]+=ballSpeed*math.cos(ballDirection*pi/180)
-    ball[1]-=ballSpeed*math.sin(ballDirection*pi/180)
-    pygame.draw.circle(screen,black,
-                       [(int)(ball[0]),(int)(ball[1])],
-                       ballRadius,0)
-
-
-    # This draws a triangle using the polygon command
-#    pygame.draw.polygon(screen,black,[[100,100],[0,200],[200,200]],5)
- 
-    # Select the font to use. Default font, 25 pt size.
     font = pygame.font.Font(None, 25)
- 
+
     # Render the text. "True" means anti-aliased text. 
     # Black is the color. This creates an image of the 
     # letters, but does not put it on the screen
     text = font.render("PyPong",True,black)
- 
+
     # Put the image of the text on the screen at 250x250
     screen.blit(text, [250,250])
  
