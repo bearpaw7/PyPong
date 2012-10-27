6855379466b8b42831a7009a6397944ec8a0d4ee
unknown <met898@L111A23.cs.tamu.edu>
2012-10-27 15:42:08 -0500

    I added my name.

--- PyPong.py
+++ PyPong.py
@@ -214,7 +214,7 @@ class PyPong:
             # Render the text. "True" means anti-aliased text. 
             # Black is the color. This creates an image of the 
             # letters, but does not put it on the screen
-            text = font.render("PyPong",True,black)
+            text = font.render("PyPong by Beef",True,black)
             score1text=font.render(player1.score(),True,red)
             score2text=font.render(player2.score(), True, blue)
         
